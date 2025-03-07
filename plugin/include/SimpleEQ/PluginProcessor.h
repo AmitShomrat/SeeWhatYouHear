2e6cf164ec310f0cdc1c643a2134d00806cdeca3
Amit <Amit56sh@gmail.com>
2025-03-07 19:01:20 +0200

    Refactor updateCutFilters update added

--- plugin/include/SimpleEQ/PluginProcessor.h
+++ plugin/include/SimpleEQ/PluginProcessor.h
@@ -98,7 +98,14 @@ private:
   void updatePeakFilter(const ChainSettings& chainSettings);
   using Coefficients = Filter::CoefficientsPtr;
   static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
-
+  template <int index, typename ChainType, typename CoefficientType>
+  void update(ChainType& cutFilter, const CoefficientType& coefficients)
+  {
+    updateCoefficients(cutFilter.template get<index>().coefficients, coefficients[index]);
+    cutFilter.template setBypassed<index>(false);
+    cutFilter.template get<index>().coefficients = *coefficients[index];
+  }
+  // Update the cut filters for the left channel
   template <typename ChainType, typename CoefficientType>
   void updateCutFilters(ChainType& leftLowCut, 
                         const CoefficientType& cutCoefficients, 
@@ -111,37 +118,53 @@ private:
 
     switch (chainSettings.lowCutSlope)
     {
-    case Slope_12:
-      leftLowCut.template setBypassed<0>(false);
-      leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-      break;
-    
-    case Slope_24:
-      leftLowCut.template setBypassed<0>(false);
-      leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-      leftLowCut.template setBypassed<1>(false);
-      leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
-      break;
-
-    case Slope_36:
-      leftLowCut.template setBypassed<0>(false);
-      leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-      leftLowCut.template setBypassed<1>(false);
-      leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
-      leftLowCut.template setBypassed<2>(false);
-      leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
-      break;
-
-    case Slope_48:
-      leftLowCut.template setBypassed<0>(false);
-      leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-      leftLowCut.template setBypassed<1>(false);
-      leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
-      leftLowCut.template setBypassed<2>(false);
-      leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
-      leftLowCut.template setBypassed<3>(false);
-      leftLowCut.template get<3>().coefficients = *cutCoefficients[3];
+      case Slope_48:
+        update<3>(leftLowCut, cutCoefficients);
+        break;
+      case Slope_36:
+        update<2>(leftLowCut, cutCoefficients);
+        break;
+      case Slope_24:
+        update<1>(leftLowCut, cutCoefficients);
+        break;
+      case Slope_12:
+        update<0>(leftLowCut, cutCoefficients);
+        break;
     }
+
+    // switch (chainSettings.lowCutSlope)
+    // {
+    // case Slope_12:
+    //   leftLowCut.template setBypassed<0>(false);
+    //   leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+    //   break;
+    
+    // case Slope_24:
+    //   leftLowCut.template setBypassed<0>(false);
+    //   leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+    //   leftLowCut.template setBypassed<1>(false);
+    //   leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+    //   break;
+
+    // case Slope_36:
+    //   leftLowCut.template setBypassed<0>(false);
+    //   leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+    //   leftLowCut.template setBypassed<1>(false);
+    //   leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+    //   leftLowCut.template setBypassed<2>(false);
+    //   leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
+    //   break;
+
+    // case Slope_48:
+    //   leftLowCut.template setBypassed<0>(false);
+    //   leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+    //   leftLowCut.template setBypassed<1>(false);
+    //   leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+    //   leftLowCut.template setBypassed<2>(false);
+    //   leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
+    //   leftLowCut.template setBypassed<3>(false);
+    //   leftLowCut.template get<3>().coefficients = *cutCoefficients[3];
+    // }
   }                        
   
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessor)
