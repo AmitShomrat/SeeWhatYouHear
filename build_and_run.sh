23e1ac84f1566c9772062bd2712fae9fab9e97f5
Amit Shomrat <Amit56sh@gmail.com>
2025-09-28 22:45:14 +0300

    removed the matrix.build_type from output path

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,7 +2,7 @@
 
 echo "Building SeeWhatYouHear VST3 plugin..."
 cd build
-cmake --build . --target SeeWhatYouHear_VST3
+cmake --build . --target SeeWhatYouHear_VST3 --verbose
 
 echo "Starting Ardour with VST3 path..."
 export VST3_PATH="/home/amits/.vst3:$VST3_PATH" # Local host artifacts location
